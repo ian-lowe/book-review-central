4ccee968bd4cc5843d5dc7bad2ac349ffb18787e
ian-lowe <ilowe@mac.com>
2019-06-27 15:38:06 -0700

    changed from tab seperated data to csv for db upload

--- import.py
+++ import.py
@@ -1,8 +1,19 @@
-import psycopg2
-conn = psycopg2.connect("host= dbname= password=")
-cur = conn.cursor()
+import csv
+import os
 
-with open('books.txt', 'r') as f:
-    next(f) # Skip the header row.
-    cur.copy_from(f, 'books', sep='\t')
-    conn.commit()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    f = open("books.csv")
+    reader = csv.reader(f)
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                    {"isbn": isbn, "title": title, "author": author, "year": year})
+    db.commit()
+
+if __name__ == "__main__":
+    main()
